d393ae9b8e324aa2742409dcf0bd992be018e68b
kana0011 <marklynoel.alvarez@whitecloak.com>
2019-07-29 19:17:19 +0800

    Create OCP example

---
+++ open-closed/src/com/whitecloak/training/badexample/CurrencyConverter.java
@@ -0,0 +1,26 @@
+package com.whitecloak.training.badexample;
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
+import java.util.Map;
+
+public class CurrencyConverter {
+
+    private final Map<Currency, BigDecimal> exchangeRate;
+
+    public CurrencyConverter(Map<Currency, BigDecimal> exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public BigDecimal converyFromPhp(BigDecimal phpAmount, Currency currency) {
+        BigDecimal quote = exchangeRate.get(currency);
+
+        if (quote != null) {
+            return phpAmount.divide(quote, RoundingMode.HALF_EVEN);
+        } else {
+            throw new IllegalArgumentException("Unsupported currency: " + currency);
+        }
+    }
+}
---
+++ open-closed/src/com/whitecloak/training/goodexample/CurrencyConverter.java
@@ -0,0 +1,20 @@
+package com.whitecloak.training.goodexample;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
+
+public class CurrencyConverter {
+
+    public BigDecimal converyFromPhp(BigDecimal phpAmount, Currency currency) {
+        if (currency.equals(Currency.getInstance("USD"))) {
+            return phpAmount.divide(new BigDecimal("51.12"), RoundingMode.HALF_EVEN);
+        } else if (currency.equals(Currency.getInstance("EUR"))) {
+            return phpAmount.divide(new BigDecimal("56.96"), RoundingMode.HALF_EVEN);
+        } else if (currency.equals(Currency.getInstance("JPY"))) {
+            return phpAmount.divide(new BigDecimal("0.47"), RoundingMode.HALF_EVEN);
+        } else {
+            throw new IllegalArgumentException("Unsupported currency: " + currency);
+        }
+    }
+}
