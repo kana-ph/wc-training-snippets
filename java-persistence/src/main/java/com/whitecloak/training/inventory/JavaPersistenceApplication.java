29bce9f083e8f9ee97730cc308a3cecdf799699d
kana0011 <marklynoel.alvarez@whitecloak.com>
2019-08-13 16:40:50 +0800

    Commit missing project files

---
+++ java-persistence/src/main/java/com/whitecloak/training/inventory/JavaPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.whitecloak.training.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPersistenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPersistenceApplication.class, args);
+    }
+
+}
