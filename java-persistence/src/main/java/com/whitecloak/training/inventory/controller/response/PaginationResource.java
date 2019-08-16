d16060b49a16552ccdbd6306480dea9d8704c78f
gabestremos <52776823+gabestremos@users.noreply.github.com>
2019-08-16 17:40:34 +0800

    Delete PaginationResource.java

--- java-persistence/src/main/java/com/whitecloak/training/inventory/controller/response/PaginationResource.java
+++
@@ -1,9 +0,0 @@
-package com.whitecloak.training.inventory.controller.response;
-
-import com.sun.tools.javac.jvm.Items;
-
-import java.util.List;
-
-public class PaginationResource {
-    private List<Items> items;
-}
